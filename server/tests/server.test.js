afcb8bce52f2042728d2ea5dfbe5f627253adc2a
nandakumarp <nandakumar1990@gmail.com>
2018-08-25 09:43:18 +0530

    Add test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,9 +5,19 @@ const {app} = require('./../server');
 const {Todo} = require('./../model/todo');
 
 var text = "Test todo addition";
+var todos = [
+    {
+        "text": "test todo 1"
+    },
+    {
+        "text": "test todo 2"
+    }
+];
 
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -25,7 +35,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -48,9 +58,22 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
+    });
+});
+
+describe('GET /todos', () => {
+    
+    it('should GET all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
     });
 });
