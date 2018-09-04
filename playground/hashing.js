d1e22f563a1a9e3df40feb8ab58d99fb0a826710
nandakumarp <nandakumar1990@gmail.com>
2018-09-04 11:29:51 +0530

    Added hashing middleware for password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,25 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$7dmPlQKVAA4.jlN9raLXlehrzc3KM8d/7xRTq67FYzdcgacSY20s6';
+
+bcrypt.compare(password, hashedPassword).then((res) => {
+    console.log('Password matched', res);
+}).catch((e) => {
+    console.log('Password match interrupted', e);
+});
+
+
+/* // Hashing using jsonwebtoken
 var data = {
     id: 10
 };
@@ -11,9 +30,9 @@ console.log(`Token ${token}`);
 
 var decoded = jwt.verify(token, 'someSaltKey');
 
-console.log('Decoded', decoded);
+console.log('Decoded', decoded); */
 
-/* SHA256 hashing and salt encoding - MANUAL OPERATION
+/* // SHA256 hashing and salt encoding - MANUAL OPERATION
 
 var message = 'im user 6';
 var hashedMessage = SHA256(message);
